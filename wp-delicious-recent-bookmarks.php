edef0dec949dfebb3ab88097ffc347bbbc615bea
Sarah Vessels <cheshire137@gmail.com>
2013-06-15 12:34:05 -0400

    Initial commit

---
+++ wp-delicious-recent-bookmarks.php
@@ -0,0 +1,25 @@
+<?php
+/*
+Plugin Name: Delicious Recent Bookmarks
+Description: Display your recent Delicious bookmarks in a widget.
+Version: 0.1
+Author: Sarah Vessels
+Author URI: http://www.3till7.net/
+License: GPLv3
+
+Copyright Sarah Vessels 2013
+
+This program is free software: you can redistribute it and/or modify
+it under the terms of the GNU General Public License as published by
+the Free Software Foundation, either version 3 of the License, or
+(at your option) any later version.
+
+This program is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License
+along with this program.  If not, see <http://www.gnu.org/licenses/>.
+*/
+?>
